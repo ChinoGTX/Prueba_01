4ad741c101b36a9b68e969076dd76cb364a16737
ChinoGTX <frikisuager@gmail.com>
2025-03-03 11:51:32 -0600

    Jajajanojalaba

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
-void main() {
-  runApp(MyApp());
+import 'package:prueba_flutter2025/pantallas/contador';
 
+
+void main() {
+  // --- debug del proyecto
+  runApp(MyApp()); //run App, llamdo a la clase MyApp
 }
 
 class MyApp extends StatelessWidget {
+  // creacion de la clase.
   @override
   Widget build(BuildContext context) {
-
+    //construimoswl widget utilizando la libreia Material.dart
     return MaterialApp(
-
+      //----Llamamos el conjunto de componentes del Material.app
+      debugShowCheckedModeBanner: false,
       home: contador(),
     );
   }
